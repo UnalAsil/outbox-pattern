7d229178befa571198f2c86c8103a506c0b405ab
UnalAsil <63313898+UnalAsil@users.noreply.github.com>
2022-06-18 21:10:46 +0300

    Creating module architure

---
+++ food-order/src/main/java/outbox/pattern/food/order/FoodOrderMain.java
@@ -0,0 +1,9 @@
+package outbox.pattern.food.order;
+
+public class FoodOrderMain {
+
+    public static void main(String[] args) {
+        //TODO-> With the domain-driven design approach, domain and infra can be separated from each other. Since it
+        // will be a simple example of outbox pattern, domain and infra were not separated from each other.
+    }
+}
